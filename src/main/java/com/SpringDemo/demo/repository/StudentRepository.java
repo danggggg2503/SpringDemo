182462a8249a9d7695a924d4d9a942d06bbc84dc
dangbinz <46754956+dangbinz@users.noreply.github.com>
2021-09-15 01:12:53 +0700

    native

--- src/main/java/com/SpringDemo/demo/repository/StudentRepository.java
+++ src/main/java/com/SpringDemo/demo/repository/StudentRepository.java
@@ -2,9 +2,16 @@ package com.SpringDemo.demo.repository;
 
 import com.SpringDemo.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long>{
+    @Query(value = "select * from Student s where s.email = ?1", nativeQuery = true)
+    public Student getStudentByEmailNative(String email);
+
+    @Query(value = "select * from Student s where s.email = :email", nativeQuery = true)
+    public Student getStudentByEmailNativeNameParam(@Param("email")String email);
 
 }
